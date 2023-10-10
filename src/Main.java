aba71bed0a343f80d7c96e6252ad457affc422ad
RaffaeleCaravetta <raffaelecaravetta13@gmail.com>
2023-10-10 14:48:25 +0200

    primo esercizio

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,52 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Invio with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Maiusc+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Ciao, scrivi un numero da 1 a 5");
+        Scanner scanner = new Scanner(System.in);
+        int nNumber = getNextNumber(scanner);
+        Set<String> stringSet = new HashSet<>();
+        scanner.nextLine();
+        System.out.println("Adesso inserisci " + nNumber + " parole:");
+        String string="";
+        Set<String> repeatedWords = new HashSet<>();
+        for (int i = 1; i <= nNumber; i++) {
+            System.out.print("Parola n° " + i + ": ");
+            string = scanner.nextLine();
+            if (stringSet.contains(string)) {
+                repeatedWords.add(string);
+            }
+            stringSet.add(string);
+        }
 
-            // Press Maiusc+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println(repeatedWords);
+        System.out.println(stringSet.size());
+        System.out.println("Elenco delle parole distinte :");
+        for(String word:stringSet){
+            System.out.println(word);
         }
     }
+    private static int getNextNumber(Scanner scanner) {
+        while (true) {
+            try { int number = scanner.nextInt();
+                if(number>0&&number<=5){
+                    return number;
+                }
+                else{
+                    scanner.nextLine();
+                    System.out.println("Inserisci un numero maggiore di 0 e minore o uguale a 5. Riprova: ");
+                }
+            } catch (java.util.InputMismatchException e) {
+                scanner.nextLine();
+                System.out.println("Il tuo numero è : " + e.getMessage());
+                System.out.print("Riprova: ");
+            }
+        }
+    }
+
 }
